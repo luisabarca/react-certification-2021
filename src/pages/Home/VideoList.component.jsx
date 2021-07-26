88275408bcb4da84a449e732597fd0992d814487
Luis Abarca <luisabarca@gmail.com>
2021-07-26 01:52:13 -0500

    WARB-MC1: Added new component to render a list of videos

---
+++ src/pages/Home/VideoList.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import Video from './Video.component';
+
+import videos from '../../data/youtube-data.json';
+
+const VideoListContainer = styled.div`
+  flex: 1;
+  display: flex;
+  margin: 0 auto;
+  padding: 20px;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const VideoList = () => {
+  const { items } = videos;
+
+  if (items.length < 0) return null;
+
+  return (
+    <VideoListContainer>
+      {items.map((item) => (
+        <Video key={item.etag} video={item} />
+      ))}
+    </VideoListContainer>
+  );
+};
+
+export default VideoList;
