1c43c18ab4392fd73cf0d5b5c34a18a313dad0d1
Luis Abarca <luisabarca@gmail.com>
2021-08-02 20:18:32 -0500

    WARB-MC3: Added provider for Search

---
+++ src/providers/SearchProvider/index.js
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from 'react';
+
+const SearchContext = React.createContext(null);
+
+export const useSearch = () => {
+  const context = useContext(SearchContext);
+
+  if (!context) {
+    throw new Error('SearchProvider not available');
+  }
+
+  return context;
+};
+
+export const SearchProvider = ({ children }) => {
+  const [query, setQuery] = useState('Wizeline');
+
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <SearchContext.Provider value={{ query, setQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchProvider;
