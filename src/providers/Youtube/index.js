5e5805af05315314437bb7a6d63a5269cf6cf851
Luis Abarca <luisabarca@gmail.com>
2021-08-22 00:39:37 -0500

    WARB-MC3: Added new Axios instance and service to consume Youtube REST API

--- src/providers/Youtube/index.js
+++ src/providers/Youtube/index.js
@@ -1,6 +1,6 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext } from 'react';
+import * as youtube from '../../services/youtube';
 
-const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 const YoutubeApiContext = React.createContext(null);
 
 export const useYoutubeApi = () => {
@@ -13,78 +13,33 @@ export const useYoutubeApi = () => {
   return context;
 };
 
-const loadClientScript = () => {
-  const script = document.createElement('script');
-  script.src = 'https://apis.google.com/js/client.js';
-
-  return script;
-};
-
 export const YouTubeApiProvider = ({ children }) => {
-  const { gapi } = window;
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    const script = loadClientScript();
-
-    script.onload = () => {
-      gapi.load('client', () => {
-        // Set API KEY.
-        gapi.client.setApiKey(YOUTUBE_API_KEY);
-        gapi.client.load('youtube', 'v3', () => {
-          setReady(true);
-        });
-      });
-    };
-
-    document.body.appendChild(script);
-  }, [gapi]);
-
   const searchVideos = useCallback(async (query) => {
-    const options = {
-      part: ['snippet'],
-      maxResults: 25,
-      q: query,
-    };
-
     try {
-      const response = await window.gapi.client.youtube.search.list(options);
-      return response.result;
+      const response = await youtube.search(query);
+      return response.data;
     } catch (error) {
       return null;
     }
   }, []);
 
   const getVideo = useCallback(async (videoId) => {
-    const options = {
-      part: ['snippet,contentDetails,statistics,player'],
-      id: videoId,
-    };
-
     try {
-      const response = await window.gapi.client.youtube.videos.list(options);
-      return response.result;
+      const response = await youtube.getVideo(videoId);
+      return response.data;
     } catch (error) {
       return null;
     }
   }, []);
 
   const getRelatedVideos = useCallback(async (videoId) => {
-    const options = {
-      part: ['snippet'],
-      relatedToVideoId: videoId,
-      type: ['video'],
-    };
-
     try {
-      const response = await window.gapi.client.youtube.search.list(options);
-      return response.result;
+      const response = await youtube.getRelatedVideos(videoId);
+      return response.data;
     } catch (error) {
       return null;
     }
   }, []);
-
-  if (!ready) return null;
 
   return (
     // eslint-disable-next-line react/jsx-filename-extension
