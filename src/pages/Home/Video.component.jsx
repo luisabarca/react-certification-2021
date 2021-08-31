bba87df7e6edb633f9810a9171a154c9e539fb98
Luis Abarca <luisabarca@gmail.com>
2021-08-31 00:48:45 -0500

    WARB-MC4: Changes for Dark theme support

--- src/pages/Home/Video.component.jsx
+++ src/pages/Home/Video.component.jsx
@@ -1,59 +1,31 @@
+import { Card, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { VideoThumbnail, VideoTitle, VideoDescription } from './styled';
 
-const VideoContainer = styled.div`
-  background-color: white;
-  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%),
-    0px 1px 3px 0px rgb(0 0 0 / 12%);
-  border-radius: 5px;
-  height: 345px;
-  margin: 10px;
-  width: 345px;
-  overflow: hidden;
-`;
-
-const VideoTitle = styled.h2`
-  font-size: 1.2rem;
-  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-  color: black;
-  font-weight: 300;
-  line-height: 1.4;
-  letter-spacing: 0.001em;
-  padding: 10px 10px 0 10px;
-  margin: 0;
-`;
-
-const VideoDescription = styled.p`
-  color: rgba(0, 0, 0, 0.54);
-  font-size: 0.8rem;
-  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-  font-weight: 400;
-  line-height: 1.43;
-  letter-spacing: 0.01em;
-  padding: 0 10px 10px 10px;
-`;
-
-const VideoThumbnail = styled.div`
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-image: url(${(props) => props.url || ''});
-  display: block;
-  height: 140px;
-`;
+const useStyles = makeStyles(() => ({
+  card: {
+    height: '345px',
+    margin: '10px',
+    width: '345px',
+  },
+}));
 
 const Video = ({ video }) => {
+  const classes = useStyles();
+
+  if (!video.id?.videoId) return null;
+
   const { thumbnails, title, description } = video.snippet;
-  const id = video.id.videoId || video.id.channelId;
+  const id = video.id.videoId;
 
   return (
     <Link to={`/video?id=${id}`}>
-      <VideoContainer>
+      <Card className={classes.card}>
         <VideoThumbnail role="img" url={thumbnails.high.url} />
         <VideoTitle>{title}</VideoTitle>
         <VideoDescription role="note">{description}</VideoDescription>
-      </VideoContainer>
+      </Card>
     </Link>
   );
 };
